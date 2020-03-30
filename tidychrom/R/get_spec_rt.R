db821bf41054553f8605ebe0295c034f973410bf
Jacob Winnikoff <jwinnikoff@mbari.org>
2020-03-30 15:34:38 -0700

    Reworked function to return scan nearest the passed RT, rather than assuming evenly spaced scans and interpolating. Slower but better.

--- tidychrom/R/get_spec_rt.R
+++ tidychrom/R/get_spec_rt.R
@@ -1,17 +1,58 @@
 # take MSnExp/raw data, sample index, and rt
 # return mass spectrum
-get_spec_rt <- function(exp, index_samp, rt){
-  # number of samples
-  num_samps <- length(fileNames(exp))
-  # number of spectra per sample; runs should be identical
-  num_spectra <- length(exp) / num_samps
-  rt_beg <- min(rtime(exp))
-  rt_end <- max(rtime(exp))
-  # which spectrum does rt correspond to?
-  index_spec <- ceiling(num_spectra * (rt - rt_beg) / (rt_end - rt_beg))
-  # put leading zeroes on indices
-  index_samp <- str_pad(index_samp, width=nchar(num_samps), side="left", pad="0")
-  index_spec <- str_pad(index_spec, width=nchar(num_spectra), side="left", pad="0")
-  feature <- paste("F", index_samp, ".S", index_spec, sep = "", collapse = "")
+# old version as of 20200330
+#get_spec_rt_interpolate <- function(exp, index_samp, rt){
+#  # number of samples
+#  num_samps <- length(fileNames(exp))
+#  # number of spectra per sample; runs should be identical
+#  num_spectra <- length(exp) / num_samps
+#  rt_beg <- min(rtime(exp))
+#  rt_end <- max(rtime(exp))
+#  # which spectrum does rt correspond to?
+#  index_spec <- ceiling(num_spectra * (rt - rt_beg) / (rt_end - rt_beg))
+#  # put leading zeroes on indices
+#  index_samp <- str_pad(index_samp, width=nchar(num_samps), side="left", pad="0")
+#  index_spec <- str_pad(index_spec, width=nchar(num_spectra), side="left", pad="0")
+#  feature <- paste("F", index_samp, ".S", index_spec, sep = "", collapse = "")
+#  return(exp[[feature]])
+#}
+
+# more robust algorithm
+# slower?
+
+# helper function extracts a tbl of feature, sample index, rtime
+exprtime2tbl <- function(exp){
+  rtime_tbl <- rtime(exp) %>%
+    bind_rows() %>%
+    gather(key = feature, value = rtime) %>%
+    # not sure why this is needed, but it is
+    rowwise() %>%
+    mutate(
+      samp_num = strsplit(feature, "\\.") %>%
+        .[[1]] %>% .[1] %>%
+        substr(., 2, str_length(.)) %>%
+        as.numeric()
+    )
+  return(rtime_tbl)
+}
+
+get_spec_rt <- function(exp, index_samp, rt, rtime_tbl){
+  # rtime_tbl can be passed into the call to save time.
+  # With a large experiment, the gather() call takes 5ever.
+  if(missing(rtime_tbl)){
+    rtime_tbl <- exprtime2tbl(exp)
+  }
+
+  feature <- rtime_tbl %>%
+    # first, filter experiment for requested sample
+    filter(samp_num == index_samp) %>%
+    # get nearest scan to query RT
+    mutate(delta = abs(rtime - rt)) %>%
+    # need to group for delta == min(delta) to work
+    group_by(1) %>%
+    # get the best-hit feature
+    filter(delta == min(delta)) %>%
+    pull(feature)
+
   return(exp[[feature]])
 }
