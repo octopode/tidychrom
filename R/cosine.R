6ae743fc73d85a5249550c7948d4b22974805ee2
Jacob Winnikoff <jwinnikoff@mbari.org>
2020-04-15 10:03:50 -0700

    Updated doc.

--- R/cosine.R
+++ R/cosine.R
@@ -1,3 +1,18 @@
+#' Cosine Similarity of Two Vectors
+#' Lifted verbatim from Fridolin Wild's LSA package (https://cran.r-project.org/web/packages/lsa/)
+#' Serves here as a helper to cosine_spectra().
+#'
+#' Takes two vector-like objects
+#' @param x vector 1
+#' @param y vector 2
+#' @return cosine similarity score of the two vector (dbl)
+#' @keywords cosine similarity vector
+#' @export
+#' @examples
+#' cos <- cosine_spectra(ions %>% filter(scan == 114), ions %>% filter(scan == 114))
+#' > 0.94
+#'
+
 ### cosine.R
 ###
 ### 2009-09-14:
@@ -12,16 +27,16 @@
 ###   * integrated cosvecs into cosine by doing type dependant processing
 ### 2005-08-26:
 ###   * rewrote cosvecs function to crossprod
-### 
+###
 
 cosine <- function( x, y=NULL ) {
-    
+
     if ( is.matrix(x) && is.null(y) ) {
-        
+
         co = array(0,c(ncol(x),ncol(x)))
         f = colnames( x )
         dimnames(co) = list(f,f)
-        
+
         for (i in 2:ncol(x)) {
             for (j in 1:(i-1)) {
                 co[i,j] = cosine(x[,i], x[,j])
@@ -29,22 +44,22 @@ cosine <- function( x, y=NULL ) {
         }
         co = co + t(co)
         diag(co) = 1
-        
+
         return (as.matrix(co))
-        
+
     } else if ( is.vector(x) && is.vector(y) ) {
         return ( crossprod(x,y) / sqrt( crossprod(x)*crossprod(y) ) )
     } else if ( is.vector(x) && is.matrix(y) ) {
-	
+
 		 co = vector(mode='numeric', length=ncol(y))
 		 names(co) = colnames(y)
 		 for (i in 1:ncol(y)) {
-			co[i] = cosine(x,y[,i]) 
+			co[i] = cosine(x,y[,i])
 		 }
 		 return(co)
-		 
+
 	 }	else {
         stop("argument mismatch. Either one matrix or two vectors needed as input.")
     }
-    
+
 }
--- R/cosine_spectra.R
+++ R/cosine_spectra.R
@@ -1,11 +1,35 @@
+#' Cosine Similarity of Two Spectra
+#'
+#' Takes two spectra in tibble form, calculates their cosine similarity score.
+#' In other words, performs a full join on the x-axis, fills with 0s, then gets cosine.
+#' @param spec_1 Spectrum 1 in tibble form, with x-axis named as passed
+#' @param spec_2 Spectrum 2 in tibble form, with x-axis named as passed
+#' @param x Name of x-coordinate column (e.g. \code{mz} or \code{wl})
+#' @return cosine similarity score of the two spectra (dbl)
+#' @keywords cosine similarity score spectra
+#' @export
+#' @examples
+#' cos <- cosine_spectra(ions %>% filter(scan == 114), ions %>% filter(scan == 114))
+#' > 0.94
+#'
+
 # Helper function: take two (binned) spectra as tibbles
 # Join them and calculate the dot product.
 cosine_spectra <- function(spec_1, spec_2, x = "mz"){
   specs <- full_join(spec_1, spec_2, by = x) %>%
+    # peaks present in 1 spectrum should be 0s in the other
     mutate(
       intensity.x = ifelse(is.na(intensity.x), 0, intensity.x),
       intensity.y = ifelse(is.na(intensity.y), 0, intensity.y)
-    )
+    ) %>%
+    # constrains comparison to the tighter mz window between the two spectra
+    filter(
+      # on the low end
+      mz >= max(min(spec_1[[x]]), min(spec_2[[x]])),
+      # on the high end
+      # this one is problematic
+      #mz <= min(max(spec_1[[x]]), max(spec_2[[x]])),
+      )
   # cosine similarity function used here is lifted from
   # Fridolin Wild's LSA package (https://cran.r-project.org/web/packages/lsa/)
   return(cosine(specs$intensity.x, specs$intensity.y)[[1]])
