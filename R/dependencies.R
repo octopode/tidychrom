61949944b7cd94dd72a212027bf4de3cb6d588f2
Jacob Winnikoff <jwinnikoff@mbari.org>
2020-04-15 11:55:13 -0700

    comments and gridExtra

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,4 +1,8 @@
+# ensure dependencies are attached
+# this may be removed at a later date
+
 library(tidyverse)
 library(xcms)
 library(pbapply) # that's parallel with progress bars
 library(ggpubr)
+library(gridExtra)
