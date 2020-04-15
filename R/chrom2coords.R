59a589c676b4db7f3af1d5dfef7dcf55ffb5c499
Jacob Winnikoff <jwinnikoff@mbari.org>
2020-04-15 11:26:20 -0700

    move old functions to their own directory

--- R/chrom2coords.R
+++
@@ -1,7 +0,0 @@
-# take a chromatogram, return a tibble with rt and intensity
-chrom2coords <- function(chrom){
-  rtime(chrom) %>%
-    cbind(intensity(chrom)) %>%
-    as_tibble() %>% setNames(c("rt", "intensity")) %>%
-    return()
-}
--- R/get_basepeak.R
+++
@@ -1,8 +0,0 @@
-# take Spectrum, return base m/z
-get_basepeak <- function(spectrum){
-  basepeak <- spec2peaklist(spectrum) %>%
-    filter(intensity == max(intensity)) %>%
-    select(mz) %>%
-    unlist() %>% unname()
-  return(basepeak)
-}
