2923e4cd9bed8ac02db300c7b0a00e2bf73a31b4
Jacob Winnikoff <jwinnikoff@mbari.org>
2020-04-15 11:57:25 -0700

    consolidated utility functions

--- R/utility.R
+++ R/utility.R
@@ -1,3 +1,12 @@
+#' Author's utility functions, to be loaded with \code{tidychrom} namespace
+
+# using devtools and roxygen2, reload the tidychrom namespace
+reload_tidychrom <- function(){
+  document()
+  detach("package:tidychrom")
+  library(tidychrom)
+}
+
 # utility function to convert JRW filename to sample code
 filename2samp <- function(filename){
   prefix <- substr(filename, 0, 3)
