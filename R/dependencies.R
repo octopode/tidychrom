286846a55ecea5159a74d0945b59960581a7cc3f
Jacob Winnikoff <jwinnikoff@mbari.org>
2020-07-01 15:14:29 -0700

    replace xcms with mzR, parallel; add igraph

--- R/dependencies.R
+++ R/dependencies.R
@@ -2,8 +2,10 @@
 # this may be removed at a later date
 
 library(tidyverse)
-library(xcms)
+library(mzR)
+library(parallel)
 library(pbapply) # that's parallel with progress bars
 library(collapse) # weighted summary stats
+library(igraph)
 library(ggpubr)
 library(gridExtra)
